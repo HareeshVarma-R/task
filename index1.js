f5a041f3091c561891ecc86db1c1d3de262e9fb5
HareeshVarma-R <74778980+HareeshVarma-R@users.noreply.github.com>
2020-11-20 21:21:16 +0530

    Delete index1.js

--- index1.js
+++
@@ -1,6 +0,0 @@
-var str="Apple, Banana, Kiwi";
-//var res=str.slice(-12,1);
-//console.log(res);
-str = "Please visit MicroSoft and Microsoft!";
-var n = str.replace(/Microsoft/gi, "W3Schools");
-console.log(n);
